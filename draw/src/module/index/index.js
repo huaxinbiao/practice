d7d0d4fb9bea62f2928a40c830dd9d451c86a50e
huaxinbiao <1960067872@qq.com>
2016-11-27 18:27:02 +0800

    VueApp

--- draw/src/module/index/index.js
+++ draw/src/module/index/index.js
@@ -7,4 +7,20 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
-this.socket = io.connect('ws://121.40.205.128:3000');
+//this.socket = io.connect('ws://121.40.205.128:3000');
+function plusReady(){
+	// 隐藏滚动条
+	plus.webview.currentWebview().setStyle({scrollIndicator:'none'});
+	// Android处理返回键
+	plus.key.addEventListener('backbutton',function(){
+		if(confirm('确认退出？')){
+			plus.runtime.quit();
+		}
+	},false);
+	compatibleAdjust();
+}
+if(window.plus){
+	plusReady();
+}else{
+	document.addEventListener('plusready',plusReady,false);
+}
