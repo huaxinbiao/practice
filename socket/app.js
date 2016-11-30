029b55103c3d207a72f762e7a32ced7b1e073bac
huaxinbiao <1960067872@qq.com>
2016-11-30 22:06:59 +0800

    MuiApp

--- socket/app.js
+++ socket/app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 var port = process.env.PORT || 3000
-
+console.log(port);
 app.use(express.static(path.join(__dirname, '/static')));//设置静态文件存放目录
 app.use(function(req, res){
     res.sendFile(path.join(__dirname, './static/index.html'))
@@ -14,6 +14,6 @@ var server = app.listen(port, function(){
 })
 var io = require('socket.io').listen(server);
 
-io.sockets.on('connection', function(){
+io.sockets.on('connection', function(socket){
     socket.emit('connected')
 })
