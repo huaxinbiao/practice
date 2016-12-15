a26a5f5e90b53dd1a0b4cb7f88d4ec8bafdd3e73
huaxinbiao <1960067872@qq.com>
2016-12-15 20:12:43 +0800

    type="clocor" ios不支持

--- muiApp/src/module/game/js/inChat.js
+++ muiApp/src/module/game/js/inChat.js
@@ -2,7 +2,13 @@
 
 Vue.component('io-chat', {
     template: `<div id="chat-scroll" v-bind:style="{top:canvasHeight+'px'}" class="mui-content Hui-chat-scroll">
-		    {{canvasHeight}}
+					<input type="color" value="#333333" list="colors">
+					<datalist id="colors">
+						<option>#ffffff</optison>
+						<option>#ff0000</option>
+						<option>#ff7700</option>
+					</datalist>
+					<input type="range" name="points" min="1" max="10" />
 			   </div>`,
 	props: ['canvasHeight'],//接收父组件传值
     mounted(){
